d28af156136a64dba172103f64157373ea57b139
Jakapoa <sben54@gmail.com>
2019-10-03 13:06:42 -0400

    memes

--- commands/iam.js
+++ commands/iam.js
@@ -26,6 +26,10 @@ exports.iam = function (logaction, message, args, botchannel, testersrole, cante
 				message.author.send(`You now have the role Madness.`);
 			}
 		}
+		else if(args.length && args[0] == 'gay') {
+			message.author.send(`You sir, you galaxy brain motherfucker, have created the best joke the world has ever been graced with. The human and potato race bows before your ability to make jokes that are the absolute pinicle of humor. Please, have mercy on us. We are but court jesters compaired to your might.`);
+			console.log('iam gay!');
+		}
 		else {
 			message.delete(10);
 			console.log('iam invalid!');
