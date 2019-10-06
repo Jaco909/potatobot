4382c78d0aac15520c30159c7ba5b708af386853
Jakapoa <sben54@gmail.com>
2019-10-06 15:00:43 -0400

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,5 +1,5 @@
 exports.changelog = function (logaction, message) {
 	logaction()
 	console.log('changelog run!');
-	message.author.send('https://github.com/Jaco909/potatobot');
+	message.author.send('https://github.com/Jaco909/potatobot/blob/master/changelog.txt');
 };
