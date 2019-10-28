9a1984a374e7811df5c71bd63557727f992b92fb
Jakapoa <sben54@gmail.com>
2019-10-28 11:45:35 -0400

    1.8.7.2
    Fixed !warn.

--- commands/warn.js
+++ commands/warn.js
@@ -24,7 +24,7 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 				time = (message.createdAt);
 				//warning for user exists maximum
 				if (fs.existsSync(`./warnings/9_${userid}.txt`)) {
-					fs.appendFileSync(`./warnings/9_${userid}.txt`, `\n**WARNING #10+**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.appendFileSync(`./warnings/9_${userid}.txt`, `\n**WARNING #10+**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 						console.log('Append complete!');
 					});
@@ -32,7 +32,8 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					client.channels.get(`${warnchannel}`).send(`**Staff:** ${message.author.username}\n**Warning:** ${user}\n**Total Warnings:** 10+\n**Reason:** ${talk.join(" ")}\n**User's Warning Log:** \`9_${userid}.txt\` \`\`\`Type !getwarn [filename] to open a user's warning file. \`\`\``);
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 				//warning for user exists 8-9
@@ -41,7 +42,7 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 						if (err) throw err;
 						console.log('Rename complete!');
 					});
-					fs.appendFileSync(`./warnings/9_${userid}.txt`, `\n**WARNING #9**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.appendFileSync(`./warnings/9_${userid}.txt`, `\n**WARNING #9**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 						console.log('Append complete!');
 					});
@@ -49,7 +50,8 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					client.channels.get(`${warnchannel}`).send(`**Staff:** ${message.author.username}\n**Warning:** ${user}\n**Total Warnings:** 9\n**Reason:** ${talk.join(" ")}\n**User's Warning Log:** \`9_${userid}.txt\` \`\`\`Type !getwarn [filename] to open a user's warning file. \`\`\``);
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 				//warning for user exists 7-8
@@ -58,7 +60,7 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 						if (err) throw err;
 						console.log('Rename complete!');
 					});
-					fs.appendFileSync(`./warnings/8_${userid}.txt`, `\n**WARNING #8**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.appendFileSync(`./warnings/8_${userid}.txt`, `\n**WARNING #8**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 						console.log('Append complete!');
 					});
@@ -66,7 +68,8 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					client.channels.get(`${warnchannel}`).send(`**Staff:** ${message.author.username}\n**Warning:** ${user}\n**Total Warnings:** 8\n**Reason:** ${talk.join(" ")}\n**User's Warning Log:** \`8_${userid}.txt\` \`\`\`Type !getwarn [filename] to open a user's warning file. \`\`\``);
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 				//warning for user exists 6-7
@@ -75,7 +78,7 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 						if (err) throw err;
 						console.log('Rename complete!');
 					});
-					fs.appendFileSync(`./warnings/7_${userid}.txt`, `\n**WARNING #7**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.appendFileSync(`./warnings/7_${userid}.txt`, `\n**WARNING #7**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 						console.log('Append complete!');
 					});
@@ -83,7 +86,8 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					client.channels.get(`${warnchannel}`).send(`**Staff:** ${message.author.username}\n**Warning:** ${user}\n**Total Warnings:** 7\n**Reason:** ${talk.join(" ")}\n**User's Warning Log:** \`7_${userid}.txt\` \`\`\`Type !getwarn [filename] to open a user's warning file. \`\`\``);
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 				//warning for user exists 5-6
@@ -92,7 +96,7 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 						if (err) throw err;
 						console.log('Rename complete!');
 					});
-					fs.appendFileSync(`./warnings/6_${userid}.txt`, `\n**WARNING #6**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.appendFileSync(`./warnings/6_${userid}.txt`, `\n**WARNING #6**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 						console.log('Append complete!');
 					});
@@ -100,7 +104,8 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					client.channels.get(`${warnchannel}`).send(`**Staff:** ${message.author.username}\n**Warning:** ${user}\n**Total Warnings:** 6\n**Reason:** ${talk.join(" ")}\n**User's Warning Log:** \`6_${userid}.txt\` \`\`\`Type !getwarn [filename] to open a user's warning file. \`\`\``);
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 				//warning for user exists 4-5
@@ -109,7 +114,7 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 						if (err) throw err;
 						console.log('Rename complete!');
 					});
-					fs.appendFileSync(`./warnings/5_${userid}.txt`, `\n**WARNING #5**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.appendFileSync(`./warnings/5_${userid}.txt`, `\n**WARNING #5**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 						console.log('Append complete!');
 					});
@@ -117,7 +122,8 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					client.channels.get(`${warnchannel}`).send(`**Staff:** ${message.author.username}\n**Warning:** ${user}\n**Total Warnings:** 5\n**Reason:** ${talk.join(" ")}\n**User's Warning Log:** \`5_${userid}.txt\` \`\`\`Type !getwarn [filename] to open a user's warning file. \`\`\``);
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 				//warning for user exists 3-4
@@ -126,7 +132,7 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 						if (err) throw err;
 						console.log('Rename complete!');
 					});
-					fs.appendFileSync(`./warnings/4_${userid}.txt`, `\n**WARNING #4**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.appendFileSync(`./warnings/4_${userid}.txt`, `\n**WARNING #4**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 						console.log('Append complete!');
 					});
@@ -134,7 +140,8 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					client.channels.get(`${warnchannel}`).send(`**Staff:** ${message.author.username}\n**Warning:** ${user}\n**Total Warnings:** 4\n**Reason:** ${talk.join(" ")}\n**User's Warning Log:** \`4_${userid}.txt\` \`\`\`Type !getwarn [filename] to open a user's warning file. \`\`\``);
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 				//warning for user exists 2-3
@@ -143,7 +150,7 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 						if (err) throw err;
 						console.log('Rename complete!');
 					});
-					fs.appendFileSync(`./warnings/3_${userid}.txt`, `\n**WARNING #3**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.appendFileSync(`./warnings/3_${userid}.txt`, `\n**WARNING #3**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 						console.log('Append complete!');
 					});
@@ -151,7 +158,8 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					client.channels.get(`${warnchannel}`).send(`**Staff:** ${message.author.username}\n**Warning:** ${user}\n**Total Warnings:** 3\n**Reason:** ${talk.join(" ")}\n**User's Warning Log:** \`3_${userid}.txt\` \`\`\`Type !getwarn [filename] to open a user's warning file. \`\`\``);
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 				//warning for user exists 1-2
@@ -160,7 +168,7 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 						if (err) throw err;
 						console.log('Rename complete!');
 					});
-					fs.appendFileSync(`./warnings/2_${userid}.txt`, `\n**WARNING #2**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.appendFileSync(`./warnings/2_${userid}.txt`, `\n**WARNING #2**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 						console.log('Append complete!');
 					});
@@ -169,13 +177,14 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					console.log('Warning logged!');
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 				//new warning user
 				else if ((!fs.existsSync(`./warnings/1_${userid}.txt`)) && (!fs.existsSync(`./warnings/2_${userid}.txt`)) && (!fs.existsSync(`./warnings/3_${userid}.txt`)) && (!fs.existsSync(`./warnings/4_${userid}.txt`)) && (!fs.existsSync(`./warnings/5_${userid}.txt`)) && (!fs.existsSync(`./warnings/6_${userid}.txt`)) && (!fs.existsSync(`./warnings/7_${userid}.txt`)) && (!fs.existsSync(`./warnings/8_${userid}.txt`)) && (!fs.existsSync(`./warnings/9_${userid}.txt`)))
 				{
-					fs.writeFileSync(`./warnings/1_${userid}.txt`, `\n**WARNING #1**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk}`, (err) => {
+					fs.writeFileSync(`./warnings/1_${userid}.txt`, `\n**WARNING #1**\n**Staff:** ${message.author.username}\n**Date:** ${time}\n**Reason:**${talk.join(" ")}`, (err) => {
 						if (err) throw err;
 					});
 					message.author.send(`${user.username}\'s warning has been logged as \`1_${userid}.txt\`. Please use \`!addwarninfo 1_${userid}.txt\` to add any additional info.`);
@@ -184,7 +193,8 @@ exports.warn = function (logaction, message, usertier, args, messageChannel, fs,
 					console.log('Warning logged!');
 					if (warnmute == 0)
 					{
-						client.users.get(`${userid}`).send(`You have recieved a warning. Reason: ${talk}.`);
+						talk == (talk.shift().toString());
+						client.users.get(`${userid}`).send(`You have recieved a warning. **Reason:** ${talk.join(" ")}.`);
 					}
 				}
 			}
